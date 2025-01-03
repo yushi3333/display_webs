b6f97d9b414a02fcd1047deba1d999fc496c443e
yushi <a1125189291@gmail.com>
2025-01-02 21:05:20 -0500

    update

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -46,11 +46,11 @@ function AppContent() {
 
   const getRecommends = async() =>{
     try{
-      const appleResponse = await axios.get('http://0.0.0.0:3002/api/products/Apple');
+      const appleResponse = await axios.get('http://127.0.0.1:3002/api/products/Apple');
 
-      const dellResponse = await axios.get('http://0.0.0.0:3002/api/products/Dell');
+      const dellResponse = await axios.get('http://127.0.0.1:3002/api/products/Dell');
 
-      const asusResponse = await axios.get('http://0.0.0.0:3002/api/products/Asus');
+      const asusResponse = await axios.get('http://127.0.0.1:3002/api/products/Asus');
       const combineRecommends = [appleResponse.data[0], dellResponse.data[0], asusResponse.data[0]]
       const combine = [...appleResponse.data, ...dellResponse.data, ...asusResponse.data]
       setRecommends(combineRecommends)
@@ -65,7 +65,7 @@ function AppContent() {
   const getDellProducts = async() =>{
     try{
       //backend api endpoint
-      const response = await axios.get("http://0.0.0.0:3002/api/products/Dell");
+      const response = await axios.get("http://127.0.0.1:3002/api/products/Dell");
       setDells(response.data)
     }catch(err){
       console.log(err)
@@ -73,7 +73,7 @@ function AppContent() {
   }
   const getAsusProducts = async() =>{
     try{
-      const response = await axios.get("http://0.0.0.0:3002/api/products/Asus");
+      const response = await axios.get("http://127.0.0.1:3002/api/products/Asus");
       setAsus(response.data)
     }catch(err){
       console.log(err)
@@ -82,7 +82,7 @@ function AppContent() {
 
   const getAppleProducts = async()=>{
     try{
-      const response = await axios.get("http://0.0.0.0:3002/api/products/Apple");
+      const response = await axios.get("http://127.0.0.1:3002/api/products/Apple");
       
       setApples(response.data)
       
